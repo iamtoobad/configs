83ff7205e0193c923d9d149ee90db78e8f0da490
hammer <hutengfei1988@gmail.com>
2014-07-02 17:19:50 +0800

    修改auto-install.sh，在本地自测通过

--- auto-install.sh
+++ auto-install.sh
@@ -14,9 +14,9 @@ die() {
 [ -e "~/.vim" ] && die "~/.vim already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-git clone https://github.com/iamtoobad/configs.git "$VIMHOME"
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/iamtoobad/configs.git $VIMHOME
+mkdir $VIMHOME/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $VIMHOME/.vimrc ~
 
 echo "hammer's vimrc is installed."
